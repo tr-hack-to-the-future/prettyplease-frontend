aba8077684bf38cf6a32ed62cc8a93ac47a855aa
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-09-22 20:03:04 +0100

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+function Main() {
+  return (
+    <div className="Main">
+      <Navbar />
+
+      <h2>Main</h2>
+
+    </div>
+  );
+}
+
+export default Main;
