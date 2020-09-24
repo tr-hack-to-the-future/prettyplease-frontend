83a40c8134f2db401889dfe1ab044f81a4496a8c
Arul-Amirtha <arul.arulnathan@gmail.com>
2020-09-24 21:06:47 +0100

    Added Number of Sponsors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Main from './Main.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [sponsor, setSponsor] = useState([
+    { sponsorName:"AlFA", completed: false},
+    { sponsorName: "Sponsor2", completed: false},
+    { sponsorName: "Black Rock", completed: false},
+    { sponsorName: "Adidas", completed: false},
+    { sponsorName: "Sponsor4", completed: true},
+    { sponsorName: "Eigen", completed: false}
+  ]);
+
   return (
     <div className="App">
 
-      <Main />
+      <Main SponsorData={sponsor}/>
 
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PPNavbar from './components/PPNavbar';
 
-function Main() {
+function Main({SponsorData}) {
   return (
     <div className="Main">
       <PPNavbar />
 
       <h2>Main</h2>
+      <p>You have {SponsorData.length} Sponsors</p>
+       {/* <p>{SponsorData} 
 
+      {SponsorData.forEach((sponsor)=> sponsor.sponsorName)}; */}
+      {/* </p> */}
     </div>
   );
 }
