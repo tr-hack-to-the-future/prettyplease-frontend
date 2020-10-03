d32105c0b3819db61ad229a5da07ece74c852a9d
leireUlacia <leulaciah@gmail.com>
2020-10-03 23:13:59 +0100

    Finish Walking Skeleton FundRequest page

--- src/components/FundRequest.js
+++ src/components/FundRequest.js
@@ -5,7 +5,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import FormCheck from 'react-bootstrap/FormCheck';
 import Button from 'react-bootstrap/Button';
 
 function FundRequest() {
