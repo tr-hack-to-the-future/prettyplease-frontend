86ea1bed590e7cf44563b677e831fdade3960cf9
Your Name <helenemartin13@hotmail.com>
2020-10-05 18:51:59 +0100

    add Carousel, Footer, Jumbroton

--- src/Main.js
+++ src/Main.js
@@ -4,18 +4,58 @@ import Sponsorlist from './components/Sponsorlist';
 import CharityPage from './components/CharityPage';
 import SponsorPage from './components/SponsorPage';
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Carousel from 'react-bootstrap/Carousel'
 
 function Main({SponsorData}) {
 
   return (
-  
+    <>
       <Jumbotron>
-        <h1>Hello, world!</h1>
+        <h1>Actions speak louder than emojis</h1>
         <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
+          Pledge and gratitude work hand in hand. Whether you are a charity or a company, find initiatives and sponsors that match your values
         </p>
       </Jumbotron>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Support Us</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
+    </>
 
   );
 }
