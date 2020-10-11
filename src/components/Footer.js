d5bdae06ed753d5c1b327230e40143650b8852d4
Your Name <helenemartin13@hotmail.com>
2020-10-11 11:57:45 +0100

    create seperate components for jumbotron and carousel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,12 @@
 import React from 'react';
 
 
+export default function Footer() {
+    return sponsor.map((ch, index) => (
+      <Link to={`/ForCharities/${index + 1}`}>
+ 
+    ));
+  }
+  
+
 export default Footer;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
-export default Jumbotron;
+export default function() {
+    return (
+        <Jumbotron>
+            <h1>Actions speak louder than emojis</h1>
+            <p>
+            Pledge and gratitude work hand in hand. Whether you are a charity or a company, find initiatives and sponsors that match your values
+            </p>
+        </Jumbotron>
+    );
+}
