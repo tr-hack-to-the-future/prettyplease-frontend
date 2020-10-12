e07c420c60dc36922bc421ccb58577352291ae51
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-12 12:52:21 +0100

    add default to formatting parameters

--- src/components/requestformatter.js
+++ src/components/requestformatter.js
@@ -1,9 +1,9 @@
 
-export function getFormattedDuration(duration) {
+export function getFormattedDuration(duration = 0) {
     return duration === "1" ? duration + " year" : duration + " years";
 }
 
-export function getFormattedAmount(amount) {
+export function getFormattedAmount(amount = 0) {
     return "£" + amount;
 }
 
