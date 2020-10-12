d6abda859890388703c7380d7bc1ecd933758fe6
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-12 10:40:47 +0100

    add render link test

--- src/components/requestdisplaycard/requestdisplaycard.test.js
+++ src/components/requestdisplaycard/requestdisplaycard.test.js
@@ -32,24 +32,29 @@ describe('DisplayCard component', () => {
         expect(getByText("Duration: 2 years")).toBeTruthy();
     })
 
-    // test(`Given the require props,
-    // When the component is rendered,
-    // Then it should render a link`, () => {
-    //     const testName = 'Test Charity';
-    //     const testDescription = 'Test description';
+    test(`Given the require props,
+    When the component is rendered,
+    Then it should render a link`, () => {
+        const testName = 'Test Charity';
+        const testDescription = 'Test description';
 
-    //     const props = {
-    //         cardData: {
-    //             charityName: testName,
-    //             description: testDescription,
-    //             amount: "£1500",
-    //             duration: "2 years"
-    //         }
-    //     }
+        const props = {
+            cardData: {
+                requestId: 13,
+                charityName: testName,
+                description: testDescription,
+                amount: "£1500",
+                duration: "2 years"
+            }
+        }
 
-    //     const { getByText } = render(<DisplayCardRequest {...props} />);
-    //     expect(getByText(testName).closest('a')).toHaveAttribute('href', 'Charity1.html');
-    // })
+        const { getByText } = render(
+            <MemoryRouter>
+                <RequestDisplayCard {...props} />
+            </MemoryRouter>
+        );
+        expect(getByText(testName).closest('a')).toHaveAttribute('href', '/ForSponsors/13');
+    })
 
 
 
