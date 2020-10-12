084e347023995eb069aeb4ae84f748c7b4a4b98b
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-12 14:10:42 +0100

    add simple App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test.skip('renders learn react link', () => {
+test('renders For Sponsors link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('For Sponsors');
   expect(linkElement).toBeInTheDocument();
 });
+
