d902c35cc724f89f09f3669a32ed5b81a153c530
Your Name <helenemartin13@hotmail.com>
2020-10-14 17:55:40 +0100

    add emojis in featurette

--- src/components/Featurette.js
+++ src/components/Featurette.js
@@ -5,21 +5,14 @@ import {Row, Col} from "react-bootstrap";
 
 export default function() {
     return (
-        <container>
-            <h1> Bouji bouji</h1>
+        <container className="featuretteContainer">
             <Row>
                 <Col sm={10}>
+                    <h4> Bouji bouji
                     <p>At Pretty P***** asking is not a taboo.  We start from the principle that if you don't ask you get. Our bespoke platform connects charities to compatible sponsors. On our platform generosity prevails.</p>
                     <p>You are a charity: Let companies wear your badge of honour, connect with the most ethical organisation globally</p>
-                    <p>you are a sponsor: you don't <span></span>where your mouth is, wear your <span></span> on your sleeve </p>
+                    <p>you are a sponsor: you don't <span role="img" aria-label="shit">💩</span> where your mouth is, wear your <span role="img" aria-label="shit">❤️</span> on your sleeve </p></h4>
                 </Col>
-                
-                {/* <Col sm={4}>
-                <h2>Actions speak louder than emojis</h2>
-                    <p>
-                    Pledge and gratitude work hand in hand. Whether you are a charity or a company, find initiatives and sponsors that match your values
-                    </p>
-                </Col> */}
             </Row>
         </container>
     );
