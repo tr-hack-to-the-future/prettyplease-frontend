353672211f0aa6fc4fd2e7ef4e38ea93298931ba
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-18 17:20:10 +0100

    add temporary navigation buttons to sponsor requests filtered by status

--- src/components/SponsorPage.js
+++ src/components/SponsorPage.js
@@ -3,43 +3,57 @@ import Card from 'react-bootstrap/Card';
 import "./SponsorPage.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import RequestDisplayCard from './requestdisplaycard/RequestDisplayCard';
 import Button from 'react-bootstrap/Button';
 import { useHistory } from 'react-router-dom';
 
 function SponsorPage({ requests }) {
 
-    const formatText = requests.length === 1 ? 'request' : 'requests';
+  const formatText = requests.length === 1 ? 'request' : 'requests';
 
-    const history = useHistory();
-    const handleClick = () => {
-        history.push("/ForSponsorsAccepted");
-    }
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/ForSponsorsAccepted");
+  }
 
-    return (
-        <div className="SponsorPage">
-            <Container>
-                <Row className="justify-content-md-center mt-4">
-                    <Button variant="outline-primary ml-5" size="lg" onClick={handleClick}>
-                        View Accepted Requests
-                    </Button>
-                </Row>
-                <Row className="justify-content-md-center mt-4">
-                    <p>There are {requests.length} sponsorship {formatText} available</p>
-                </Row>
+  return (
+    <div className="SponsorPage">
+      <Container>
+        {/* TODO temporary buttons for navigation - will be moved into Navbar */}
+        <Row className="justify-content-md-center mt-4"> 
+        <Col>
+            <Button variant="outline-primary ml-5" size="lg" disabled>
+              Open Requests
+            </Button>
+          </Col>          
+          <Col>
+            <Button variant="outline-primary ml-5" size="lg" onClick={handleClick}>
+              Accepted Requests
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="outline-primary ml-5" size="lg" >
+              Declined Requests
+            </Button>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center mt-4">
+          <p>There are {requests.length} sponsorship {formatText} available</p>
+        </Row>
 
-                <Row>
-                    <Card>
-                        <RequestDisplayCard cardData={requests} />
-                    </Card>
+        <Row>
+          <Card>
+            <RequestDisplayCard cardData={requests} />
+          </Card>
 
-                </Row>
+        </Row>
 
-            </Container>
-        </div>
+      </Container>
+    </div>
 
 
-    );
+  );
 }
 
 export default SponsorPage;
--- src/components/sponsorpageaccepted/SponsorPageAccepted.js
+++ src/components/sponsorpageaccepted/SponsorPageAccepted.js
@@ -2,16 +2,42 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import RequestAcceptedDisplayCard from '../requestaccepteddisplaycard/RequestAcceptedDisplayCard';
+import { useHistory } from 'react-router-dom';
 import "./SponsorPageAccepted.css";
 
 function SponsorPageAccepted({ requests }) {
 
-    const formatText = requests.length === 1 ? 'request' : 'requests';
+    const history = useHistory();
+    const handleClick = () => {
+        history.push("/ForSponsors");
+    }
 
+
+    const formatText = requests.length === 1 ? 'request' : 'requests';
     return (
         <div className="SponsorPageAccepted">
             <Container>
+                {/* TODO temporary buttons for navigation - will be moved into Navbar */}
+                <Row className="justify-content-md-center mt-4">
+                    <Col>
+                        <Button variant="outline-primary ml-5" size="lg" onClick={handleClick}>
+                            Open Requests
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button variant="outline-primary ml-5" size="lg" disabled>
+                            Accepted Requests
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button variant="outline-primary ml-5" size="lg" >
+                            Declined Requests
+                        </Button>
+                    </Col>
+                </Row>
                 <Row className="justify-content-md-center mt-4">
                     <p>You have {requests.length} ACCEPTED sponsorship {formatText}</p>
                 </Row>
