9ccb0d0ea62087dbd172579021d8b0f030f5930e
Your Name <helenemartin13@hotmail.com>
2020-10-19 12:24:26 +0100

    add styled component and babel plugin styled component, create a footer component

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,19 @@
-import React, { Component, useState } from 'react';
-import Sponsorlist from './components/Sponsorlist';
-import CharityPage from './components/CharityPage';
-import SponsorPage from './components/SponsorPage';
-import Jumbotron from './components/Jumbotron';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
-import Featurette from './components/Featurette';
-import './App.css';
+import React, { Component, useState } from "react";
+import Sponsorlist from "./components/Sponsorlist";
+import CharityPage from "./components/CharityPage";
+import SponsorPage from "./components/SponsorPage";
+import Jumbotron from "./components/Jumbotron";
+import Carousel from "./components/Carousel";
+import Featurette from "./components/Featurette";
+import "./App.css";
 
-
-function Main({SponsorData}) {
-
+function Main({ SponsorData }) {
   return (
     <>
       <Jumbotron />
       <Featurette />
       <Carousel />
-      <Footer />
     </>
-
   );
 }
 
---
+++ src/components/footer/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function Footer({ children, direction = 'row', ...restProps })
