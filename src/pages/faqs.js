f9db18de978c9b26836971b6ac82da48737635c9
Your Name <helenemartin13@hotmail.com>
2020-10-21 10:01:06 +0100

    fix path to route FAQS

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { FooterContainer } from "../containers/footer";
+import { FaqsContainer } from "../containers/faqs";
 
 export default function FaqsPage() {
   return (
