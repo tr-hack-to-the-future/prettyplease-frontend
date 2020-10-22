b582f815c396df138620b88c6c027b3519ec6080
leireUlacia <leulaciah@gmail.com>
2020-10-23 00:44:34 +0100

    Updates

---
+++ precache-manifest.7c9dae5885f0b797e30b9f933087ecec.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73a58faed8f1860d1ffeaf117c110a1",
+    "url": "/prettyplease-frontend/index.html"
+  },
+  {
+    "revision": "ecbf55487c86adbed2ef",
+    "url": "/prettyplease-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dc71190c79b455addbfa",
+    "url": "/prettyplease-frontend/static/css/main.bc1b0d51.chunk.css"
+  },
+  {
+    "revision": "ecbf55487c86adbed2ef",
+    "url": "/prettyplease-frontend/static/js/2.2951b163.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/prettyplease-frontend/static/js/2.2951b163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc71190c79b455addbfa",
+    "url": "/prettyplease-frontend/static/js/main.c07b865f.chunk.js"
+  },
+  {
+    "revision": "c07f7f86c38865880859",
+    "url": "/prettyplease-frontend/static/js/runtime-main.c638165f.js"
+  },
+  {
+    "revision": "f1947bbd5a6e3443a0244657fab132a4",
+    "url": "/prettyplease-frontend/static/media/999club.f1947bbd.jpg"
+  },
+  {
+    "revision": "c12ae83accb90a5eb1262a97f694285b",
+    "url": "/prettyplease-frontend/static/media/abstract-logo.c12ae83a.jpg"
+  },
+  {
+    "revision": "61e6e747a6d4f4656b161b3b69caad60",
+    "url": "/prettyplease-frontend/static/media/abstract-logo2.61e6e747.jpg"
+  },
+  {
+    "revision": "d4d0f2c9209822b1c7c87eca90f5d293",
+    "url": "/prettyplease-frontend/static/media/abstract-logo3.d4d0f2c9.jpg"
+  },
+  {
+    "revision": "053a4a7437b3ce5ab0e04289811359db",
+    "url": "/prettyplease-frontend/static/media/abstract-logo4.053a4a74.jpg"
+  },
+  {
+    "revision": "4c725eeb1778a9de615f856c6a63ce19",
+    "url": "/prettyplease-frontend/static/media/charity-logo-orange.4c725eeb.png"
+  },
+  {
+    "revision": "f5cc36c4432868c197d5dd9829a531ac",
+    "url": "/prettyplease-frontend/static/media/hackneyfoodbank.f5cc36c4.jpg"
+  },
+  {
+    "revision": "887e16b38ed6d700def3f963c3212740",
+    "url": "/prettyplease-frontend/static/media/kindness.887e16b3.jpg"
+  },
+  {
+    "revision": "73033f1adce650e35c2a2592f6729f42",
+    "url": "/prettyplease-frontend/static/media/literoflight.73033f1a.jpg"
+  },
+  {
+    "revision": "4b8e969cf800e203cd9be1a4e97d68ed",
+    "url": "/prettyplease-frontend/static/media/mind.4b8e969c.jpg"
+  },
+  {
+    "revision": "1a926e7a17ba473a179f32a632a2ec09",
+    "url": "/prettyplease-frontend/static/media/plasticoceans.1a926e7a.jpg"
+  }
+]);
