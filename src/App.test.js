631b17d9ad2d6403b9c4bc4efd03e79019f67d10
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-24 14:44:24 +0100

    update app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
+// need to mock the useEffect hooks 
 test.skip('renders For Sponsors link', () => {
-  const { getByText } = render(<App />);
+  act(() => {
+    const { getByText } = render(<App />);
   const linkElement = getByText('For Sponsors');
   expect(linkElement).toBeInTheDocument();
+  });
+
 });
