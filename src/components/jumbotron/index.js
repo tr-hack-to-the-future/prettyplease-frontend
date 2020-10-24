0112744d5a6dbb3593f1d5c4d16dfbac359913fa
Your Name <helenemartin13@hotmail.com>
2020-10-24 20:41:26 +0100

    in jumbotron component, add styles folder, nested in it a jumbotron.js file

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Container,
+  Item,
+  Inner,
+  Title,
+  SubTitle,
+  Image
+} from "./styles/jumbotron";
+
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Item = styled.div``;
+
+export const Container = styled.section``;
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
