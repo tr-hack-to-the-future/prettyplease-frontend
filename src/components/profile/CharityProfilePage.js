0d38d1d0a775c98bcf80eeae9e44074d3cc2cf26
leireUlacia <leulaciah@gmail.com>
2020-10-24 22:14:49 +0100

    Add container to Sponsor and Charity Page and delete FooterContainer in faqs

--- src/components/profile/CharityProfilePage.js
+++ src/components/profile/CharityProfilePage.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProfileTextDescription from './ProfileTextDescription';
@@ -7,12 +8,14 @@ import ProfileTextDescription from './ProfileTextDescription';
 function CharityProfilePage({charityData, changeProfile}) {   
     return (     
         <div>
+            <Container>
                  <Row  className="justify-content-md-center">
                      <Col xs={{span: 12, offset: 6}} md={{span: 6, offset: 5}}>
                         <h1>Profile</h1>
                     </Col>
                 </Row>
             <ProfileTextDescription detailsProfile={charityData} changeProfile={changeProfile}/>
+            </Container>
         </div>
     );
 }
