bbf0ae9e2aaac5ad77b8a3305cdb6f8589d39352
Your Name <helenemartin13@hotmail.com>
2020-10-25 12:29:41 +0000

    create campaigns page, routing page in navbar

---
+++ src/containers/campaigns.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function CampaignsContainer() {
+  return <div>Hello</div>;
+}
---
+++ src/pages/campaigns.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterContainer } from "../containers/footer";
+import { CampaignsContainer } from "../containers/campaigns";
+
+function Campaigns() {
+  return (
+    <>
+      <FooterContainer />
+    </>
+  );
+}
+
+export default Campaigns;
