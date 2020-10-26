e32a6ec33269db0e38d57caa4b11f34f63e1f95f
Your Name <helenemartin13@hotmail.com>
2020-10-26 19:52:15 +0000

    got rid of featurette, included it in a section in the jumbotron, add lodash depency to prevent callback for transitioning navbar on scroll, use throttled effect, add style for the onscroll effect

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Accordion } from "./accordion";
 export { default as Jumbotron } from "./jumbotron";
-export { default as Featurette } from "./featurette";
 export { default as Footer } from "./footer";
