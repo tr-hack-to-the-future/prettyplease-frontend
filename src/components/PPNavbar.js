ae3c8ea91fe0d9524b4f88ec19465031dbfd533b
leireUlacia <leulaciah@gmail.com>
2020-10-26 00:54:07 +0000

    Change default radio button FundRequest, Add style to Confirmation page, add userType to Navbar

--- src/components/PPNavbar.js
+++ src/components/PPNavbar.js
@@ -6,6 +6,12 @@ import Nav from "react-bootstrap/Nav";
 import { HashRouter as Router } from "react-router-dom";
 
 function PPNavbar({ isAuth }) {
+
+  const currentUser = {
+    uid: "b19dcdc9-1547-11eb-9ed1-0a7222284ed8",
+    userType: "charity"
+  }
+
   return (
     <Router>
       <Navbar className="navbar" collapseOnSelect expand="lg">
@@ -24,40 +30,41 @@ function PPNavbar({ isAuth }) {
             <Nav.Link href="/prettyplease-frontend/#/campaigns">
               Campaigns
             </Nav.Link>
-            <Nav.Link href="/prettyplease-frontend/#/CharityProfilePage">
-              Charity Profile
-            </Nav.Link>
-            <Nav.Link href="/prettyplease-frontend/#/SponsorProfilePage">
-              Sponsor Profile
-            </Nav.Link>
           </Nav>
           <Nav>
             {!isAuth ? (
-              // <Nav.Link href="/profile/CharityProfilePage">Login</Nav.Link>
-              <Nav.Link href="">Login</Nav.Link>
+
+            currentUser.userType === "charity" ?
+                <Nav.Link href="/prettyplease-frontend/#/CharityProfilePage">Profile</Nav.Link> :
+                <Nav.Link href="/prettyplease-frontend/#/SponsorProfilePage">Profile</Nav.Link>
+              // if (currentUser.userType=== "sponsor"){
+              //   <Nav.Link href="/prettyplease-frontend/#/CharityProfilePage">Profile</Nav.Link>
+              // }else if (currentUser.userType=== "charity" ){
+              //   <Nav.Link href="/prettyplease-frontend/#/SponsorProfilePage">Profile</Nav.Link>
+              // } 
             ) : (
-              <Nav.Link href="">
-                <svg
-                  width="1em"
-                  height="40px"
-                  viewBox="0 0 16 16"
-                  className="bi bi-person-circle"
-                  fill="#000000"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"
-                  />
-                </svg>
-                Profile
-              </Nav.Link>
-            )}
+                <Nav.Link href="">
+                  <svg
+                    width="1em"
+                    height="40px"
+                    viewBox="0 0 16 16"
+                    className="bi bi-person-circle"
+                    fill="#000000"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"
+                    />
+                  </svg>
+                Login
+                </Nav.Link>
+              )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
