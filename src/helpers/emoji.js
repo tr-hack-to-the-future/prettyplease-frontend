166c30deb929e5d782591f4ce5cd86663b107889
Your Name <helenemartin13@hotmail.com>
2020-10-26 16:23:29 +0000

    add functionality to the emoji props: add padding right to fit evenly within text

--- src/helpers/emoji.js
+++ src/helpers/emoji.js
@@ -3,9 +3,11 @@ import React from "react";
 const Emoji = props => (
   <span
     className="emoji"
+    style={{ paddingRight: "5px" }}
     role="img"
     aria-label={props.label ? props.label : ""}
     aria-hidden={props.label ? "false" : "true"}
+    paddingRight="40px"
   >
     {props.symbol}
   </span>
