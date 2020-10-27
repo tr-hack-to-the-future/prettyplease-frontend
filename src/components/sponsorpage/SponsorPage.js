e148cc2f02ebf689d03cbe69a6840d003c1c39ac
leireUlacia <leulaciah@gmail.com>
2020-10-27 16:43:07 +0000

    Add buttons of Sponsors to the navbar. Style the sponsor cards

--- src/components/sponsorpage/SponsorPage.js
+++ src/components/sponsorpage/SponsorPage.js
@@ -3,9 +3,7 @@ import Card from 'react-bootstrap/Card';
 import "./SponsorPage.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import RequestDisplayCard from '../requestdisplaycard/RequestDisplayCard';
-import Button from 'react-bootstrap/Button';
 import { useHistory } from 'react-router-dom';
 import { getFormattedPlural } from '../requestformatter';
 import { useAuth } from "../Firebase/AuthContext";
@@ -13,12 +11,6 @@ import { useAuth } from "../Firebase/AuthContext";
 function SponsorPage() {
 
   const history = useHistory();
-  const handlePending = () => {
-    history.push("/ForSponsorsPending");
-  }
-  const handleAccepted = () => {
-    history.push("/ForSponsorsAccepted");
-  }
 
   // // const {currentUser} = useAuth();
   // const currentUser = {
@@ -57,26 +49,9 @@ function SponsorPage() {
   return (
     <div className="SponsorPage">
       <Container>
-        {/* TODO temporary buttons for navigation - will be moved into Navbar */}
+
         <Row className="justify-content-md-center mt-4">
-          <Col>
-            <Button variant="outline-primary ml-5" size="lg" disabled>
-              Requests
-            </Button>
-          </Col>
-          <Col>
-            <Button variant="outline-primary ml-5" size="lg" onClick={handlePending}>
-              Offers Pending
-          </Button>
-          </Col>
-          <Col>
-            <Button variant="outline-primary ml-5" size="lg" onClick={handleAccepted}>
-              Offers Accepted
-            </Button>
-          </Col>
-        </Row>
-        <Row className="justify-content-md-center mt-4">
-          <p>There are {requests.length} sponsorship {getFormattedPlural("request", requests.length)} available</p>
+          <h5>There are {requests.length} sponsorship {getFormattedPlural("request", requests.length)} available</h5>
         </Row>
 
         <Row>
--- src/components/sponsorpagepending/SponsorPagePending.js
+++ src/components/sponsorpagepending/SponsorPagePending.js
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import OfferPendingDisplayCard from '../offerpendingdisplaycard/OfferPendingDisplayCard';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from "../Firebase/AuthContext";
@@ -15,12 +13,6 @@ import { getFormattedPlural } from '../requestformatter';
 function SponsorPagePending() {
 
     const history = useHistory();
-    const handleAccepted = () => {
-        history.push("/ForSponsorsAccepted");
-    }
-    const handleRequests = () => {
-        history.push("/ForSponsors");
-    }
 
     // fetch the sponsor offers    
     let { offers, getOffers } = useAuth();
@@ -37,29 +29,12 @@ function SponsorPagePending() {
     return (
         <div className="SponsorPagePending">
             <Container>
-                {/* TODO temporary buttons for navigation - will be moved into Navbar */}
+
                 <Row className="justify-content-md-center mt-4">
-                    <Col>
-                        <Button variant="outline-primary ml-5" size="lg" onClick={handleRequests}>
-                            Requests
-                        </Button>
-                    </Col>
-                    <Col>
-                        <Button variant="outline-primary ml-5" size="lg" disabled>
-                            Offers Pending
-                        </Button>
-                    </Col>
-                    <Col>
-                        <Button variant="outline-primary ml-5" size="lg" onClick={handleAccepted}>
-                            Offers Accepted
-                        </Button>
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-center mt-4">
-                    <p>You have {offersPending.length} pending sponsorship {getFormattedPlural("offer", offersPending.length)}</p>
+                    <h5>You have {offersPending.length} pending sponsorship {getFormattedPlural("offer", offersPending.length)}</h5>
                 </Row>
 
-                <Row>
+                <Row className="card-SponsorPagePending">
                     <Card>
                         <OfferPendingDisplayCard cardData={offersPending} />
                     </Card>
