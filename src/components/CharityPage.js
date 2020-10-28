800bd200bc3372ce01c45d9f74e39e0d0c62813e
Arul-Amirtha <arul.arulnathan@gmail.com>
2020-10-28 13:34:37 +0000

    Fixing charity page sponsor received

--- src/components/CharityPage.js
+++ src/components/CharityPage.js
@@ -22,9 +22,8 @@ function CharityPage() {
       <div className="ReviewAccept">
         <Row className="justify-content-md-center mt-4">
           <h3>
-            {openOffers.length > 1
-              ? "You have received {openOffers.length} offers"
-              : "We will update here as soon as you receive offers!"}
+            {openOffers.length ===0 ?"We will update here as soon as you receive offers!"
+              : (openOffers.length===1?"You have received an offer":"You have received "+ openOffers.length+" offers")} 
           </h3>
         </Row>
 
